5fc26650242bf0fdad7ee8055529f51b36ab4430
Esterfano Lopes <esterfano.lopes@gmail.com>
2021-12-07 07:42:33 -0300

    45. Types of numbers

---
+++ 6.NumbersAndVariableTypes/src/NumberTypes.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var items = 5
+    println(items::class.java)
+
+    var nbOfPeople = 750000000000
+    println(nbOfPeople::class.java)
+
+    var pi = 3.141592635
+    println(pi::class.java)
+}
