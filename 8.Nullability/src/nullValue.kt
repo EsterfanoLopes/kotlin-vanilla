28654f84595f5b3fd10a133c81cdd4142d98268c
Esterfano Lopes <esterfano.lopes@gmail.com>
2021-12-07 08:08:15 -0300

    73. Null value

---
+++ 8.Nullability/src/nullValue.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val herName: String = "Lilly"
+    val hisName: String? = null
+    println(herName)
+    println(hisName)
+
+    var catName: String? = "Chonkers"
+    catName = null
+
+    // Can't assign a null value to a variable that is not allowed to receive null
+    // var dogName: String = "Fluffy"
+    // dogName = null
+}
