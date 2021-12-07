72fc538717b839cee0ee13066cb9a6992f129da2
Esterfano Lopes <esterfano.lopes@gmail.com>
2021-12-07 08:36:58 -0300

    88. Finally

---
+++ 9.Exceptions/src/finally.kt
@@ -0,0 +1,13 @@
+import java.lang.Exception
+
+fun main(args: Array<String>) {
+    val test = readLine()
+    try {
+        println(test?.toInt())
+    } catch (e: Exception) {
+        e.printStackTrace()
+        println("An exception happened \n ${e.localizedMessage}")
+    } finally {
+        println("The execution has completed")
+    }
+}
